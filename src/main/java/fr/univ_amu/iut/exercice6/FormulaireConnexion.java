b7fb90c63a6490ad81722757c5b522fe096307a8
BULUT-Melissa-25006748 <melissa.bulut@etu.univ-amu.fr>
2026-06-12 14:31:32 +0000

    Exercice 6 terminé

--- src/main/java/fr/univ_amu/iut/exercice6/FormulaireConnexion.java
+++ src/main/java/fr/univ_amu/iut/exercice6/FormulaireConnexion.java
@@ -1,11 +1,15 @@
 package fr.univ_amu.iut.exercice6;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -59,6 +63,34 @@ public class FormulaireConnexion extends Application {
     // 4. Ajouter les handlers okClicked() et cancelClicked().
     //
     // 5. Créer la Scene, l'attacher au Stage, afficher.
+    GridPane grille = new GridPane();
+    grille.setPadding(new Insets(20));
+    grille.setHgap(10);
+    grille.setVgap(10);
+    userId = new TextField();
+    userId.setId("user-id");
+    pwd = new PasswordField();
+    pwd.setId("pwd");
+    okBtn = new Button("OK");
+    okBtn.setId("btn-ok");
+    cancelBtn = new Button("Annuler");
+    cancelBtn.setId("btn-cancel");
+    message = new Label();
+    message.setId("message");
+    grille.add(new Label("Identifiant :"), 0, 0);
+    grille.add(userId, 1, 0);
+    grille.add(new Label("Mot de passe :"), 0, 1);
+    grille.add(pwd, 1, 1);
+    grille.add(okBtn, 0, 2);
+    grille.add(cancelBtn, 1, 2);
+    grille.add(message, 0, 3, 2, 1);
+    createBindings();
+    okBtn.setOnAction(event -> okClicked());
+    cancelBtn.setOnAction(event -> cancelClicked());
+    Scene scene = new Scene(grille, 350, 200);
+    primaryStage.setTitle("Connexion");
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   /** Crée les bindings de validation. */
@@ -81,14 +113,49 @@ public class FormulaireConnexion extends Application {
     //    - dans computeValue() : retourner true si le mot de passe est
     //      trop court (< 8) OU ne contient pas de majuscule OU pas de chiffre
     //    okBtn.disableProperty().bind(binding)
+    pwd.editableProperty().bind(Bindings.greaterThanOrEqual(userId.textProperty().length(), 6));
+    cancelBtn
+        .disableProperty()
+        .bind(
+            Bindings.and(
+                Bindings.equal(0, pwd.textProperty().length()),
+                Bindings.equal(0, userId.textProperty().length())));
+    BooleanBinding isPasswordInvalid =
+        new BooleanBinding() {
+          {
+            super.bind(pwd.textProperty());
+          }
+
+          @Override
+          protected boolean computeValue() {
+            String password = pwd.getText();
+            if (password == null || password.length() < 8) {
+              return true;
+            }
+
+            boolean hasUppercase = false;
+            boolean hasDigit = false;
+            for (char c : password.toCharArray()) {
+              if (Character.isUpperCase(c)) hasUppercase = true;
+              if (Character.isDigit(c)) hasDigit = true;
+            }
+            return !(hasUppercase && hasDigit);
+          }
+        };
+    okBtn.disableProperty().bind(isPasswordInvalid);
   }
 
   void okClicked() {
     // TODO exercice 6 : afficher l'identifiant et le mot de passe masqué.
+    String maskedPwd = "*".repeat(pwd.getText().length());
+    message.setText("Connecté ! User : " + userId.getText() + " | Pwd : " + maskedPwd);
   }
 
   void cancelClicked() {
     // TODO exercice 6 : vider les deux champs et le label message.
+    userId.clear();
+    pwd.clear();
+    message.setText("");
   }
 
   public static void main(String[] args) {
