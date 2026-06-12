4a8d31833675b7fd4c931f1992d8b282517e9724
BULUT-Melissa-25006748 <melissa.bulut@etu.univ-amu.fr>
2026-06-12 15:14:17 +0000

    feat: exercice 8 - convertisseur temperatures capstone

--- src/main/java/fr/univ_amu/iut/exercice8/ConvertisseurTemperatures.java
+++ src/main/java/fr/univ_amu/iut/exercice8/ConvertisseurTemperatures.java
@@ -1,6 +1,17 @@
 package fr.univ_amu.iut.exercice8;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.converter.NumberStringConverter;
 
@@ -49,6 +60,77 @@ public class ConvertisseurTemperatures extends Application {
     //        new NumberStringConverter())
     //
     // 6. Composer les panneaux dans un HBox, créer la Scene, afficher.
+    Label labelC = new Label("Celsius (°C)");
+    labelC.setMaxWidth(Double.MAX_VALUE);
+    labelC.setStyle(
+        "-fx-background-color: #4a90d9; -fx-text-fill: white; -fx-font-weight: bold; -fx-font-size: 16px; -fx-alignment: center; -fx-padding: 10;");
+    Slider sliderC = new Slider(0, 100, 0);
+    sliderC.setId("slider-celsius");
+    sliderC.setOrientation(Orientation.VERTICAL);
+    sliderC.setShowTickMarks(true);
+    sliderC.setShowTickLabels(true);
+    sliderC.setMajorTickUnit(10);
+    TextField tfC = new TextField();
+    tfC.setId("tf-celsius");
+    tfC.setMaxWidth(50);
+    tfC.setTextFormatter(new TextFormatter<>(new NumberStringConverter()));
+    Label labelF = new Label("Fahrenheit (°F)");
+    labelF.setMaxWidth(Double.MAX_VALUE);
+    labelF.setStyle(
+        "-fx-background-color: #e74c3c; -fx-text-fill: white; -fx-font-weight: bold; -fx-font-size: 16px; -fx-alignment: center; -fx-padding: 10;");
+    Slider sliderF = new Slider(32, 212, 32);
+    sliderF.setId("slider-fahrenheit");
+    sliderF.setOrientation(Orientation.VERTICAL);
+    sliderF.setShowTickMarks(true);
+    sliderF.setShowTickLabels(true);
+    sliderF.setMajorTickUnit(20);
+    TextField tfF = new TextField();
+    tfF.setId("tf-fahrenheit");
+    tfF.setMaxWidth(50);
+    tfF.setTextFormatter(new TextFormatter<>(new NumberStringConverter()));
+    Bindings.bindBidirectional(
+        tfC.textProperty(), sliderC.valueProperty(), new NumberStringConverter());
+    Bindings.bindBidirectional(
+        tfF.textProperty(), sliderF.valueProperty(), new NumberStringConverter());
+    sliderC
+        .valueProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              if (!updating) {
+                updating = true;
+                sliderF.setValue(newVal.doubleValue() * 9.0 / 5.0 + 32);
+                updating = false;
+              }
+            });
+    sliderF
+        .valueProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              if (!updating) {
+                updating = true;
+                sliderC.setValue((newVal.doubleValue() - 32) * 5.0 / 9.0);
+                updating = false;
+              }
+            });
+    VBox vboxC = new VBox(15, labelC, sliderC, tfC);
+    vboxC.setAlignment(Pos.TOP_CENTER);
+    vboxC.setPrefWidth(170);
+    vboxC.setStyle(
+        "-fx-border-color: #bdc3c7; -fx-border-width: 1; -fx-background-color: #f8f9fa;");
+    vboxC.setPadding(new Insets(0, 0, 15, 0)); // Padding en bas pour décoller le TextField
+    VBox vboxF = new VBox(15, labelF, sliderF, tfF);
+    vboxF.setAlignment(Pos.TOP_CENTER);
+    vboxF.setPrefWidth(170);
+    vboxF.setStyle(
+        "-fx-border-color: #bdc3c7; -fx-border-width: 1; -fx-background-color: #f8f9fa;");
+    vboxF.setPadding(new Insets(0, 0, 15, 0));
+    HBox root = new HBox(20, vboxC, vboxF);
+    root.setAlignment(Pos.CENTER);
+    root.setPadding(new Insets(20));
+    Scene scene = new Scene(root, 400, 400);
+    primaryStage.setTitle("Convertisseur de Températures");
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
